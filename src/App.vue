bb5a4bc74bc350ec4356b329002957ee1193b6e1
tchduo <37461622+tchduo@users.noreply.github.com>
2018-08-28 14:33:54 -0700

    start

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <el-button></el-button>
+  </div>
+</template>
+
+<script>
+
+import ElButton  from './ui/button/ElButton.vue';
+
+export default {
+  name: 'app',
+
+  components: { ElButton },
+
+  data () {
+    return {
+      
+    }
+  }
+}
+/**
+ * import sass: https://stackoverflow.com/questions/46491510/vue-js-separate-css-scss-from-the-single-vue-file
+ * 
+ * 
+  <style lang="scss">  ?? add scoped?
+    @import './foo.scss';
+  </style>
+ */
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/ui/button/ElButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button 
+    class="el-button"
+  >
+    simple buttons
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ElButton'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './ElButton.scss';
+</style>
+
+
