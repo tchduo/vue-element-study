ac1826ce943f8b891beb11ffc623315cf0e4c9bb
tchduo <37461622+tchduo@users.noreply.github.com>
2018-09-05 19:46:59 -0700

    working on alert.

--- src/ui/alert/ElAlert.vue
+++ src/ui/alert/ElAlert.vue
@@ -1,5 +1,35 @@
 <template>
-  
+  <transition name="el-alert-fade">
+    <div
+      class="el-alert"
+      :class="[typeclass, center ? 'is-center' : '']"
+      v-show="visible"
+      role="alert"
+    >
+      <i class="el-alert__icon" 
+        :class="[iconClass, isBigIcon]"
+        v-if="showIcon"
+      >
+      </i>
+      <div class="el-alert__content">
+        <span
+          class="el-alert__title"
+          :class="[ isBoldTitle ]"
+          v-if="title"
+        >
+          {{ title }}
+        </span>
+        <slot>
+          <p class="el-alert__description" v-if="description">
+            {{ description }}
+          </p>
+        </slot>
+        <i></i>
+      </div> 
+
+
+    </div>
+  </transition>
 </template>
 
 <script>
