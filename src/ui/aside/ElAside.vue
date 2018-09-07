c981664f1c3ffba35090629d4aaa3232f1604f9a
tchduo <37461622+tchduo@users.noreply.github.com>
2018-09-06 20:34:14 -0700

    working on aside.

---
+++ src/ui/aside/ElAside.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <aside
+    class="el-aside"
+    :style="{ width: width }"
+  >
+    <slot></slot>
+  </aside>
+
+</template>
+
+<script>
+export default {
+  name: 'ElAside',
+
+  props: {
+    width: {
+      type: String,
+      default: '300px'
+    }
+  }
+};
+</script>
+
